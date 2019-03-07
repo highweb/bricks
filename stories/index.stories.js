4d3eb829d190d90b6043f96051ecce1e4288ef17
Anton Staroverov <by@tonystar.me>
2019-03-07 23:30:08 +0100

    Up examples

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -10,6 +10,7 @@ import button from '../src/components/button/button.twig'
 import carousel from '../src/components/atomic-carousel/atomic-carousel.twig'
 import gallery from '../src/components/atomic-gallery/atomic-gallery.twig'
 import galleryCarousel from '../src/components/examples/gallery-carousel/gallery-carousel.twig'
+import eventsCarousel from '../src/components/examples/events-carousel/events-carousel.twig'
 
 const stories = storiesOf('Demo', module)
 
@@ -64,3 +65,34 @@ const galleryData = {
 stories
   .add('gallery', () => gallery({ gallery: galleryData }))
   .add('gallery-carousel', () => galleryCarousel({ gallery: galleryData }))
+  .add('events-carousel', () => eventsCarousel({ events: [
+    {
+      date: '10 September 1960',
+      title: '<h3>Righteous indignation</h3>',
+      description: '<p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment.</p>',
+      gallery: galleryData
+    },
+    { date: '20 May 1970',
+      title: 'Nam libero tempore',
+      description: '<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>',
+      gallery: galleryData
+    },
+    {
+      date: '30 September 2000',
+      title: 'Lorem ipsum dolor sit amet',
+      description: '<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>',
+      gallery: galleryData
+    },
+    {
+      date: 'now',
+      title: 'koa Sünd Edlweiss',
+      description: '<p>Hoid auf der Oim, da gibt’s koa Sünd Edlweiss, Haberertanz. Ned woar luja mim allerweil oba hi Leonhardifahrt! I bin a woschechta Bayer i daad scheans nomoi moand oans.</p>',
+      gallery: galleryData
+    },
+    {
+      date: '+2 months',
+      title: 'Freibia Engelgwand',
+      description: '<p>Almrausch owe griaß God beinand, jedza abfieseln wo hi griasd eich midnand Broadwurschtbudn Freibia Engelgwand fensdaln! Woibbadinga schnacksln resch Jodler, mechad wos.</p>',
+      gallery: galleryData
+    }
+  ]}))
